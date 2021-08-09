2ee816c5f1fc3164845034da3a6794d7e39df7cf
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-09 09:52:28 -0700

    fixed the display homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Homepage from "./Homepage";
+import Contentpage from "./Contentpage";
+import Typewriter from "typewriter-effect";
+
+import { FaFastForward } from "react-icons/fa";
+import { IconContext } from "react-icons";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Homepage />
+        </Route>
+        <Route path="/content">
+          <Contentpage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
