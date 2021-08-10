1b4c7c63c494985ca873a2d20fec3b4568970ade
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-10 03:47:39 -0700

    updated to fix refresh problem

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 //for nav bar implementation
@@ -11,9 +11,21 @@ import "./navbar.css";
 //5. if u chnage ur url manually to /content/project itwill show the content on page right but active component is not changing as well as
 //6+ if u refresh page it going to set the active item in navbar as about aas its the default value when the component is rendered again
 //7 to avoid this behaviour we need to remove theabout from usestate as initinal state
-//8.
+//8. so now  whatever is there in browser has to be in  page so we take advantage of browser location href and use useEffect hook from react
+// After implementing hook even after reshig the page the link and page matter will be in sync with but if user types something wrong in link the
+// we need to provide a default path to home using redirect component from react dom router
+
 export default function Navbar() {
-  const [active, setActive] = useState("About");
+  const [active, setActive] = useState();
+
+  useEffect(() => {
+    let currentURL = window.location.href;
+    console.log(currentURL);
+    if (currentURL.endsWith("/content")) setActive("About");
+    else if (currentURL.endsWith("/content/protfolio")) setActive("Portfolio");
+    else if (currentURL.endsWith("/content/resume")) setActive("Resume");
+    else if (currentURL.endsWith("/content/contact")) setActive("Contact");
+  }, [active]);
 
   return (
     <div className="Navbar d-flex justify-content-between">
