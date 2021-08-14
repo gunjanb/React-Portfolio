d25db3d56f46991c53a431c084df8c27a4022a89
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-13 23:49:44 -0700

    updated for project cards and search based on technology used

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import projectdata from "./data/projectdata";
+import ProjectCard from "./ProjectCard";
+import "./portfolio.css";
 
 export default function Portfolio() {
-  return <div>in portfolio</div>;
+  const [project, setProjects] = useState(projectdata);
+
+  const handleFilterCategory = (name) => {
+    const searchedResults = projectdata.filter((project) =>
+      project.category.includes(name)
+    );
+    setProjects(searchedResults);
+  };
+  return (
+    <div className="container portfolio">
+      <div className="project-navbar d-flex justify-content-start ">
+        <div className="p-2" onClick={() => setProjects(projectdata)}>
+          All
+        </div>
+        <div className="p-2" onClick={() => handleFilterCategory("React.js")}>
+          React
+        </div>
+        <div className="p-2" onClick={() => handleFilterCategory("MongoDB")}>
+          MongoDB
+        </div>
+        <div className="p-2" onClick={() => handleFilterCategory("Node.js")}>
+          Node
+        </div>
+        <div className="p-2" onClick={() => handleFilterCategory("JS")}>
+          JS
+        </div>
+        <div className="p-2" onClick={() => handleFilterCategory("MySQL")}>
+          MySQL
+        </div>
+      </div>
+      <div className="row">
+        {project.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))}
+      </div>
+    </div>
+  );
 }
