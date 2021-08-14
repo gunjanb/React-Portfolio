f4afec418d6291b85bcebc528f8f29367d01c73a
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-13 23:47:39 -0700

    updated contact form and toaster

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,178 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+// import emailjs from "emailjs-com";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import "./contact.css";
 
-export default function Contact() {
-  return <div>in contact</div>;
-}
+const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  // Function that displays a success toast on top right of the page when form submission is successful
+  const toastifySuccess = () => {
+    toast("Contact information has been submitted !", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: "submit-feedback success",
+      toastId: "notifyToast",
+    });
+  };
+
+  // Function called on submit that uses emailjs to send email of valid contact form
+  const onSubmit = async (data) => {
+    // Destrcture data object
+    const { name, email, subject, message } = data;
+    try {
+      const templateParams = {
+        name,
+        email,
+        subject,
+        message,
+      };
+
+      // await emailjs.send(
+      //   process.env.REACT_APP_SERVICE_ID,
+      //   process.env.REACT_APP_TEMPLATE_ID,
+      //   templateParams,
+      //   process.env.REACT_APP_USER_ID
+      // );
+
+      reset();
+      toastifySuccess();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className="ContactForm d-flex justify-content-center m-5">
+      <div className="container">
+        <div
+          className="row p-4"
+          style={{ backgroundColor: "rgb(0 0 0 / 15%)" }}
+        >
+          <div className="col-12 text-center">
+            <div className="contactForm">
+              <form
+                id="contact-form"
+                onSubmit={handleSubmit(onSubmit)}
+                noValidate
+              >
+                {/* Row 1 of form */}
+                <div className="row formRow m-3">
+                  <div className="col-6">
+                    <input
+                      type="text"
+                      name="name"
+                      {...register("name", {
+                        required: {
+                          value: true,
+                          message: "Please enter your name",
+                        },
+                        maxLength: {
+                          value: 30,
+                          message: "Please use 30 characters or less",
+                        },
+                      })}
+                      className="form-control formInput"
+                      placeholder="Name"
+                    ></input>
+                    {errors.name && (
+                      <span className="errorMessage">
+                        {errors.name.message}
+                      </span>
+                    )}
+                  </div>
+                  <div className="col-6">
+                    <input
+                      type="email"
+                      name="email"
+                      {...register("email", {
+                        required: true,
+                        pattern:
+                          /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                      })}
+                      className="form-control formInput"
+                      placeholder="Email address"
+                    ></input>
+                    {errors.email && (
+                      <span className="errorMessage">
+                        Please enter a valid email address
+                      </span>
+                    )}
+                  </div>
+                </div>
+                {/* Row 2 of form */}
+                <div className="row formRow m-3">
+                  <div className="col">
+                    <input
+                      type="text"
+                      name="subject"
+                      {...register("subject", {
+                        required: {
+                          value: true,
+                          message: "Please enter a subject",
+                        },
+                        maxLength: {
+                          value: 75,
+                          message: "Subject cannot exceed 75 characters",
+                        },
+                      })}
+                      className="form-control formInput"
+                      placeholder="Subject"
+                    ></input>
+                    {errors.subject && (
+                      <span className="errorMessage">
+                        {errors.subject.message}
+                      </span>
+                    )}
+                  </div>
+                </div>
+                {/* Row 3 of form */}
+                <div className="row formRow m-3">
+                  <div className="col">
+                    <textarea
+                      rows={3}
+                      name="message"
+                      {...register("message", {
+                        required: true,
+                      })}
+                      className="form-control formInput"
+                      placeholder="Message"
+                    ></textarea>
+                    {errors.message && (
+                      <span className="errorMessage">
+                        Please enter a message
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <button
+                  className="submit-btn"
+                  type="submit "
+                  style={{
+                    borderRadius: "0.5rem",
+                    padding: ".5rem",
+                  }}
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+            <ToastContainer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
