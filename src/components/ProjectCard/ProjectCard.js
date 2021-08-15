e81cfc4f87b6b0fb9361273b27c2ce90adf79f9a
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-14 19:05:41 -0700

    fixed responsiveness

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -7,7 +7,8 @@ const ProjectCard = ({
   project: { name, image, deployed_url, github_url },
 }) => {
   return (
-    <div className="projectrCard col-md-6 col-lg-4 my-2">
+    // my-2
+    <div className="projectrCard col-md-6 col-lg-4  my-2">
       <div className="projectCard-wrapper">
         <a href={deployed_url} target="_blank" rel="noopener noreferrer">
           <img src={image} alt={name} className="projectCard-image img-fluid" />
