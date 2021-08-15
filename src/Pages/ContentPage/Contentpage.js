da4a3d9662523eafde23e312948b86d6eadb7deb
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-14 18:56:36 -0700

    updated for responsiveness and animation

--- src/Pages/ContentPage/Contentpage.js
+++ src/Pages/ContentPage/Contentpage.js
@@ -5,10 +5,12 @@ import About from "../../components/About/About";
 import Portfolio from "../../components/Portfolio/Portfolio";
 import Resume from "../../components/Resume/Resume";
 import Contact from "../../components/Contact/Contact";
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Redirect, Switch, useLocation } from "react-router-dom";
 import "./contentpage.css";
+import { AnimatePresence } from "framer-motion";
 
 function Contentpage() {
+  const location = useLocation();
   return (
     <Container fluid className="pt-4 min-vh-100 content">
       <Row className="row-height">
@@ -20,27 +22,29 @@ function Contentpage() {
           {/* </div> */}
         </Col>
         {/* <Col sm={12} md={9} lg={9}> */}
-        <Col lg={9}>
+        <Col lg={9} className="main-component">
           <div className="content-custom-styles py-3 px-2 rounded hover-shadow">
             {/* navbar */}
             <Navbar />
-            <Switch>
-              <Route exact path="/content">
-                <About />
-              </Route>
-              <Route path="/content/portfolio">
-                <Portfolio />
-              </Route>
-              <Route path="/content/resume">
-                <Resume />
-              </Route>
-              <Route path="/content/contact">
-                <Contact />
-              </Route>
-              <Route>
-                <Redirect to path="/content" />
-              </Route>
-            </Switch>
+            <AnimatePresence exitBeforeEnter>
+              <Switch location={location} key={location.key}>
+                <Route exact path="/content">
+                  <About />
+                </Route>
+                <Route path="/content/portfolio">
+                  <Portfolio />
+                </Route>
+                <Route path="/content/resume">
+                  <Resume />
+                </Route>
+                <Route path="/content/contact">
+                  <Contact />
+                </Route>
+                <Route>
+                  <Redirect to path="/content" />
+                </Route>
+              </Switch>
+            </AnimatePresence>
           </div>
         </Col>
         {/* <div className="col-3 sidebar">Hi</div>
