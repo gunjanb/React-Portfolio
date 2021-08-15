a61117f7b98ce71a7a417c05660a554d1697bcd7
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-08-14 19:03:48 -0700

    fixed responsiveness for small screen and added animation

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,6 +2,26 @@ import React, { useState } from "react";
 import projectdata from "../data/projectdata";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import "./portfolio.css";
+import { motion } from "framer-motion";
+
+const portfolio_variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.2,
+      duration: 0.6,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      ease: "easeOut",
+    },
+  },
+};
 
 export default function Portfolio() {
   const [project, setProjects] = useState(projectdata);
@@ -13,24 +33,48 @@ export default function Portfolio() {
     setProjects(searchedResults);
   };
   return (
-    <div className="container portfolio">
+    <motion.div
+      className="container portfolio"
+      variants={portfolio_variants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
       <div className="project-navbar d-flex justify-content-start ">
-        <div className="p-2" onClick={() => setProjects(projectdata)}>
+        <div
+          className="proj-navbar-item"
+          onClick={() => setProjects(projectdata)}
+        >
           All
         </div>
-        <div className="p-2" onClick={() => handleFilterCategory("React.js")}>
+        <div
+          className="proj-navbar-item"
+          onClick={() => handleFilterCategory("React.js")}
+        >
           React
         </div>
-        <div className="p-2" onClick={() => handleFilterCategory("MongoDB")}>
+        <div
+          className="proj-navbar-item"
+          onClick={() => handleFilterCategory("MongoDB")}
+        >
           MongoDB
         </div>
-        <div className="p-2" onClick={() => handleFilterCategory("Node.js")}>
+        <div
+          className="proj-navbar-item"
+          onClick={() => handleFilterCategory("Node.js")}
+        >
           Node
         </div>
-        <div className="p-2" onClick={() => handleFilterCategory("JS")}>
+        <div
+          className="proj-navbar-item"
+          onClick={() => handleFilterCategory("JS")}
+        >
           JS
         </div>
-        <div className="p-2" onClick={() => handleFilterCategory("MySQL")}>
+        <div
+          className="proj-navbar-item"
+          onClick={() => handleFilterCategory("MySQL")}
+        >
           MySQL
         </div>
       </div>
@@ -39,6 +83,6 @@ export default function Portfolio() {
           <ProjectCard key={project.name} project={project} />
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
